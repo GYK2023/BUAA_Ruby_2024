0f853b80968f8a7085f6183a576d55f07bd63c38
root <root@Guo0322.localdomain>
2024-12-12 22:11:55 +0800

    修改host

--- project/config/environments/development.rb
+++ project/config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.hosts << "http://22371214.project.rubyapp.act.buaa.edu.cn:9000"
+  config.hosts << "22371214.project.rubyapp.act.buaa.edu.cn"
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.enable_reloading = true
 
